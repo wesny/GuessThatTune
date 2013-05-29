593026a0aacd2eee0f6efa0df8592b92a2105a9b
iDropout <rbyip1@gmail.com>
2013-05-29 11:08:26 -0300

    Create Contestant.java

---
+++ Contestant.java
@@ -0,0 +1,31 @@
+public class Contestant{
+
+    private String _name;
+    private Score _myScore;
+
+    public Contestant(){
+  _name = "bob";
+	_myScore = 0;
+}
+
+    public Contestant(String name){
+	_name = name;
+	_myScore = 0;
+    }
+    
+    public Score getScore(){
+	return _myScore;
+    }
+
+    public Score addScore(){ return _myScore.add();}
+
+    public Score subtractScore(){return _myScore.subtract;}
+    
+    public String getName(){ return _name;}
+
+    public String changeName(String newName){
+	String temp = _name;
+	_name = newName;
+	return temp;
+    }
+}
