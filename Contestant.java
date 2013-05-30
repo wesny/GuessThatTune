b115d8000d1212df2855125a9709caede67649e5
arinabykadorova <arinaostr@gmail.com>
2013-05-30 09:07:08 -0300

    Update Contestant.java

--- Contestant.java
+++ Contestant.java
@@ -2,26 +2,39 @@ public class Contestant{
 
     private String _name;
     private Score _myScore;
+    private int _numRounds;
 
     public Contestant(){
         _name = "bob";
 	_myScore = new Score();
+	_numRounds = 0;
     }
 
     public Contestant(String name){
+	this();
 	_name = name;
-	_myScore = new Score();
     }
     
     public Score getScore(){
 	return _myScore;
     }
 
-    public Score addScore(){ return _myScore.add();}
+    public Score addScore(){ 
+    	return _myScore.add();
+    }
 
-    public Score subtractScore(){return _myScore.subtract();}
+    public Score subtractScore(){
+    	return _myScore.subtract();
+    }
     
-    public String getName(){ return _name;}
+    public String getName(){ 
+    	return _name;
+    }
+
+    public int getNumRounds(){
+	return _numRounds;		
+    }	
+
 
     public String changeName(String newName){
 	String temp = _name;
