ab225eb3f1e79c8204b07da3d0cafa1ed27c151c
wesny <sweyn3@gmail.com>
2013-06-03 08:13:27 -0400

    Renamed class file name
    minor change

--- Buttonswithreturns.java
+++ Buttonswithreturns.java
@@ -7,12 +7,12 @@
  *
  * @author WaqarulIslam
  */
-public class NewJFrame extends javax.swing.JFrame {
+public class Buttonswithreturns extends javax.swing.JFrame {
 int guess;
     /**
-     * Creates new form NewJFrame
+     * Creates new form Buttonswithreturns
      */
-    public NewJFrame() {
+    public Buttonswithreturns() {
         initComponents();
     }
 
@@ -145,20 +145,20 @@ int guess;
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Buttonswithreturns.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Buttonswithreturns.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Buttonswithreturns.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Buttonswithreturns.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new NewJFrame().setVisible(true);
+                new Buttonswithreturns().setVisible(true);
             }
         });
     }
